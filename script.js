7281f6e43ea0c630abaf306951bd1de534d72d0a
GabrielAlcantara01 <141934868+GabrielAlcantara01@users.noreply.github.com>
2023-09-21 10:14:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function validar(login, senha) {
     if (login == "admin" && senha == "admin") {
-        location.href = "https://www.uninove.br/";
+        location.href = "https://gabrielalcantara01.github.io/cronometro/";
     } else { alert('Usuario e/ou senha incorretas') }
 }
